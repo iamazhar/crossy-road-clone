3184897ab7c6f738ce35dabc56d48bb5facbe1a9
Azhar <azharcodes@gmail.com>
2019-02-05 18:14:02 +0530

    Added vehicle nodes to the sceneView

--- JumpyLane/LaneNode.swift
+++ JumpyLane/LaneNode.swift
@@ -12,9 +12,25 @@ enum LaneType {
     case grass, road
 }
 
+class TrafficNode: SCNNode {
+    let type: Int
+    let directionRight: Bool
+    
+    init(type: Int, directionRight: Bool) {
+        self.type = type
+        self.directionRight = directionRight
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class LaneNode: SCNNode {
     
     let type: LaneType
+    var trafficNode: TrafficNode?
     
     init(type: LaneType, width: CGFloat) {
         self.type = type
@@ -26,6 +42,8 @@ class LaneNode: SCNNode {
             createLane(width: width, height: 0.4, image: texture)
         case .road:
             guard let texture = UIImage(named: "art.scnassets/asphalt.png") else { break }
+            trafficNode = TrafficNode(type: Int(arc4random_uniform(3)), directionRight: randomBool(odds: 2))
+            addChildNode(trafficNode!)
             createLane(width: width, height: 0.05, image: texture)
         }
     }
@@ -40,6 +58,53 @@ class LaneNode: SCNNode {
         
         let laneNode = SCNNode(geometry: laneGeometry)
         addChildNode(laneNode)
+        addElements(width, laneNode)
+    }
+    
+    func addElements(_ width: CGFloat, _ laneNode: SCNNode) {
+        
+        var carGap = 0
+        
+        for index in 0 ..< Int(width) {
+            if type == .grass {
+                if randomBool(odds: 7) {
+                    let vegetation = getVegetation()
+                    vegetation.position = SCNVector3(10 - Float(index), 0, 0)
+                    laneNode.addChildNode(vegetation)
+                }
+            } else if type == .road {
+                carGap += 1
+                if carGap > 3 {
+                    guard let trafficNode = trafficNode else { continue}
+                    if randomBool(odds: 4) {
+                        carGap = 0
+                        let vehicle = getVehicle(for: trafficNode.type)
+                        vehicle.position = SCNVector3(10 - Float(index), 0, 0)
+                        let yAngle: CGFloat = toRadians(angle: 180)
+                        vehicle.eulerAngles = trafficNode.directionRight ? SCNVector3Zero : SCNVector3(0, yAngle, 0)
+                        trafficNode.addChildNode(vehicle)
+                    }
+                }
+            }
+        }
+    }
+    
+    func getVegetation() -> SCNNode {
+        let vegetation = randomBool(odds: 2) ? Models.tree.clone() : Models.hedge.clone()
+        return vegetation
+    }
+    
+    func getVehicle(for type: Int) -> SCNNode {
+        switch type {
+        case 0:
+            return Models.car.clone()
+        case 1:
+            return Models.blueTruck.clone()
+        case 2:
+            return Models.firetruck.clone()
+        default:
+            return Models.car.clone()
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
--- JumpyLane/Utils.swift
+++ JumpyLane/Utils.swift
@@ -9,6 +9,23 @@
 import Foundation
 import SceneKit
 
+struct Models {
+    private static let treeScene = SCNScene(named: "art.scnassets/Tree.scn")!
+    static let tree = treeScene.rootNode.childNode(withName: "tree", recursively: true)!
+    
+    private static let hedgeScene = SCNScene(named: "art.scnassets/Hedge.scn")!
+    static let hedge = hedgeScene.rootNode.childNode(withName: "hedge", recursively: true)!
+    
+    private static let carScene = SCNScene(named: "art.scnassets/PurpleCar.scn")!
+    static let car = carScene.rootNode.childNode(withName: "car", recursively: true)!
+    
+    private static let blueTruckScene = SCNScene(named: "art.scnassets/BlueTruck.scn")!
+    static let blueTruck = blueTruckScene.rootNode.childNode(withName: "truck", recursively: true)!
+    
+    private static let firetruckScene = SCNScene(named: "art.scnassets/Firetruck.scn")!
+    static let firetruck = firetruckScene.rootNode.childNode(withName: "truck", recursively: true)!
+}
+
 let degreesPerRadians = Float(Double.pi/180)
 let radiansPerDegress = Float(180/Double.pi)
 
